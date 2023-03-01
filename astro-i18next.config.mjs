83e96a732d5f9b641e2e53bae3498ea649774c31
Alireza Safaierad <frontendmonster@gmail.com>
2023-03-01 18:36:14 +0400

    remove IranSans font

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,8 +1,14 @@
+import locales from './astro.mjs';
+
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
   defaultLocale: 'en',
   locales: ['en', 'fa'],
   i18nextClient: {
     debug: true,
+    resources: locales,
+  },
+  i18nextServerPlugins: {
+    fsBackend: null,
   },
 };
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,8 @@ import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 import astroI18next from 'astro-i18next';
 
-import locales from './astro.mjs';
-
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
   integrations: [astroI18next()],
-  i18nextServer: {
-    debug: true,
-    resources: locales,
-  },
-  i18nextServerPlugins: {
-    fsBackend: null,
-  },
 });
