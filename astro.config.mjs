36a1dae8084e4d0b706014eaefcfd088c323755e
Alireza Safaierad <frontendmonster@gmail.com>
2023-03-01 18:17:08 +0400

    fix: fix build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,16 @@ import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 import astroI18next from 'astro-i18next';
 
+import locales from './astro.mjs';
+
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
   integrations: [astroI18next()],
+  i18nextServer: {
+    resources: locales,
+  },
+  i18nextServerPlugins: {
+    fsBackend: null,
+  },
 });
---
+++ astro.mjs
@@ -0,0 +1,6 @@
+import en from './public/locales/en/translation.json' assert { type: 'json' };
+import fa from './public/locales/fa/translation.json' assert { type: 'json' };
+
+const locales = { en, fa };
+
+export default locales;
