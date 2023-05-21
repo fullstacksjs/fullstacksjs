207226f2a0b00fd76e94c79b8357107047961267
Alireza Safaierad <frontendmonster@gmail.com>
2023-05-21 16:53:03 +0400

    add react plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
+import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 import astroI18next from 'astro-i18next';
 
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [astroI18next(), tailwind()],
+  integrations: [astroI18next(), tailwind(), react()],
 });
