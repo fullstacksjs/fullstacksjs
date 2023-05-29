271b2bb158253aa12a64e0d0c596cbf7cd486436
Alireza Safaierad <frontendmonster@gmail.com>
2023-05-30 02:04:48 +0400

    add google tag manager

--- src/env.d.ts
+++ src/env.d.ts
@@ -8,6 +8,7 @@ interface ImportMetaEnv {
   readonly PUBLIC_AUTH0_DOMAIN: string;
   readonly PUBLIC_AUTH0_CLIENT_ID?: string;
   readonly PUBLIC_GA_TRACKING_ID?: string;
+  readonly PUBLIC_GTM_CONTAINER?: string;
 }
 
 interface ImportMeta {
