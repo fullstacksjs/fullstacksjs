ac6edb1dbd7bf79c28995c5b1fa8709c870d660a
Alireza Safaierad <frontendmonster@gmail.com>
2023-05-30 01:44:13 +0400

    add google analytics

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="astro/client" />
+/// <reference types="gtag.js" />
+
+// eslint-disable-next-line no-var
+declare var dataLayer: any[];
+
+interface ImportMetaEnv {
+  readonly PUBLIC_AUTH0_DOMAIN: string;
+  readonly PUBLIC_AUTH0_CLIENT_ID?: string;
+  readonly PUBLIC_GA_TRACKING_ID?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
