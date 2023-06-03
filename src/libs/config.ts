0194615a66f248894608bb6bb88441a3c14aa04a
Alireza Safaierad <frontendmonster@gmail.com>
2023-06-03 18:49:24 +0400

    add analytics config

--- src/libs/config.ts
+++ src/libs/config.ts
@@ -5,6 +5,10 @@ const Config = z.object({
     domain: z.string(),
     clientId: z.string(),
   }),
+  analytics: z.object({
+    containerId: z.string(),
+    trackingId: z.string(),
+  }),
 });
 
 export type Config = z.infer<typeof Config>;
@@ -13,5 +17,9 @@ export const config = Config.parse({
   auth0: {
     domain: import.meta.env.PUBLIC_AUTH0_DOMAIN,
     clientId: import.meta.env.PUBLIC_AUTH0_CLIENT_ID,
+  },
+  analytics: {
+    containerId: import.meta.env.PUBLIC_GTM_CONTAINER,
+    trackingId: import.meta.env.PUBLIC_GA_TRACKING_ID,
   },
 } as Config);
