c4c9411e38cca5652648d3eb2e68870675bed32d
masoudnemati <masoud.nnr@gmail.com>
2023-07-26 23:46:27 +0330

    create unique title and description for every page

--- src/app/[locale]/ask/page.tsx
+++ src/app/[locale]/ask/page.tsx
@@ -4,6 +4,33 @@ import type { Locales } from '@/i18n';
 import { getMessages } from '@/i18n';
 
 import AskContent from './AskContent';
+import { Metadata } from 'next';
+import { OpenGraph } from 'next/dist/lib/metadata/types/opengraph-types';
+
+const title =
+  'How to Ask a Programming Question: A Guide to Getting the Help You Need';
+const description =
+  'This guide will teach you how to ask a programming question in a way that is clear, concise, and informative. By following these tips, you can increase your chances of getting the help you need from other programmers.';
+const ogImage: OpenGraph['images'] = {
+  url: '/og/og.png',
+  alt: title,
+};
+
+export const metadata: Metadata = {
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    images: ogImage,
+  },
+  twitter: {
+    title,
+    description,
+    images: ogImage,
+    card: 'summary_large_image',
+  },
+};
 
 export default async function AskPage() {
   const locale = useLocale() as Locales;
--- src/app/[locale]/rules/page.tsx
+++ src/app/[locale]/rules/page.tsx
@@ -4,6 +4,32 @@ import type { Locales } from '@/i18n';
 import { getMessages } from '@/i18n';
 
 import RulesContent from './RulesContent';
+import { Metadata } from 'next';
+import { OpenGraph } from 'next/dist/lib/metadata/types/opengraph-types';
+
+const title = 'FullstacksJS Community Rules: A Guide to Respectful Conduct';
+const description =
+  'The FullstacksJS community is committed to creating a safe and welcoming environment for everyone. These rules outline the behaviors that are considered harassment and unacceptable within our community. By following these rules, we can all help to create a positive and productive environment for learning and collaboration.';
+const ogImage: OpenGraph['images'] = {
+  url: '/og/og.png',
+  alt: title,
+};
+
+export const metadata: Metadata = {
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    images: ogImage,
+  },
+  twitter: {
+    title,
+    description,
+    images: ogImage,
+    card: 'summary_large_image',
+  },
+};
 
 export default async function RulesPage() {
   const locale = useLocale() as Locales;
