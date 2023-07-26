0953f1035ee22825ba42a48a6bec1bcd180b6b5e
Alireza Safaierad <frontendmonster@gmail.com>
2023-07-26 15:05:14 +0400

    add guild og

--- src/app/[locale]/guild/page.tsx
+++ src/app/[locale]/guild/page.tsx
@@ -4,6 +4,32 @@ import type { Locales } from '@/i18n';
 import { getMessages } from '@/i18n';
 
 import { GuildContent } from './GuildContent';
+import { Metadata } from 'next';
+import { OpenGraph } from 'next/dist/lib/metadata/types/opengraph-types';
+
+const title = 'FullstacksJS - TypeScript Guild';
+
+const description = 'We Grow together';
+const ogImage: OpenGraph['images'] = {
+  url: '/og/guild.png',
+  alt: 'FullstacksJS - TypeScript Guild',
+};
+
+export const metadata: Metadata = {
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    images: ogImage,
+  },
+  twitter: {
+    title,
+    description,
+    images: ogImage,
+    card: 'summary_large_image',
+  },
+};
 
 export default async function GuildPage() {
   const locale = useLocale();
