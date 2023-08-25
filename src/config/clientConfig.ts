a071a47b18b04d4e3a241582c94e44deb0310bba
Alireza Safaierad <frontendmonster@gmail.com>
2023-08-25 21:23:14 +0400

    remove firebase config

--- src/config/clientConfig.ts
+++ src/config/clientConfig.ts
@@ -10,16 +10,6 @@ const ClientConfig = z.object({
     url: z.string(),
     key: z.string(),
   }),
-
-  firebase: z.object({
-    apiKey: z.string(),
-    authDomain: z.string(),
-    projectId: z.string(),
-    storageBucket: z.string(),
-    messagingSenderId: z.string(),
-    appId: z.string(),
-    measurementId: z.string().optional(),
-  }),
 });
 
 export type ClientConfig = z.infer<typeof ClientConfig>;
@@ -30,15 +20,5 @@ export const clientConfig = ClientConfig.parse({
   supabase: {
     url: process.env.NEXT_PUBLIC_SUPABASE_URL,
     key: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  },
-
-  firebase: {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
   },
 });
