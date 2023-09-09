07cf5b1cd08eebdfd240591d95985e3e7d4313a7
abxlfazl <abxlfazlkhxrshidi@gmail.com>
2023-09-09 22:55:54 +0330

    add type page api

--- src/app/[locale]/type/+components/Timer.tsx
+++ src/app/[locale]/type/+components/Timer.tsx
@@ -1,9 +1,41 @@
 import { formatStopWatch } from '@/utils/date';
+import { useAsyncEffect } from 'ahooks';
+import { useAtom } from 'jotai';
 
-import { useTimeEllipses } from '../atoms';
+import { isFinishedAtom, useTimeEllipses, recordAtom } from '../atoms';
+import { useSupabase } from '@/data-layer/supabase/SupabaseProvider';
 
 export const Timer = ({ className }: { className?: string }) => {
   const diff = useTimeEllipses();
+  const [isFinished] = useAtom(isFinishedAtom);
+  const { supabase } = useSupabase();
+  const [record, setRecord] = useAtom(recordAtom);
 
-  return <span className={className}>{formatStopWatch(diff)}</span>;
+  useAsyncEffect(async () => {
+    const session = (await supabase.auth.getSession()).data.session;
+
+    if (session === null) return;
+
+    const lastRecord = (await supabase.from('records').select('best_record'))
+      .data?.[0]?.best_record;
+    const bestRecord =
+      diff && lastRecord ? Math.min(diff, lastRecord) : lastRecord || diff;
+
+    setRecord(bestRecord);
+
+    await supabase.from('records').upsert(
+      {
+        best_record: bestRecord,
+        user_id: session.user.id,
+      },
+      { onConflict: 'user_id' },
+    );
+  }, [isFinished]);
+
+  return (
+    <>
+      <span className={className}>{formatStopWatch(diff)}</span>
+      {record && <span>your best time {formatStopWatch(record)}</span>}
+    </>
+  );
 };
