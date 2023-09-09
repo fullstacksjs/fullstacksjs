39b2ea68e9ccfde955d7dfec8e5c28a9841f7f65
Alireza Safaierad <frontendmonster@gmail.com>
2023-09-09 17:06:06 +0400

    add deploy supabase workflow

--- src/data-layer/supabase/SupabaseServer.tsx
+++ src/data-layer/supabase/SupabaseServer.tsx
@@ -40,14 +40,16 @@ export const getProfile = cache(async (): Promise<Profile | null> => {
   }
 });
 
-export const getSubscription = cache(async () => {
+export const getSubscription = cache(async (): Promise<boolean> => {
   const supabase = createServerSupabaseClient();
   const session = await getSession();
+
+  if (!session) return false;
 
   const { data } = await supabase
     .from('subscription')
     .select('*')
-    .eq('user_id', session?.user.id)
+    .eq('user_id', session.user.id)
     .eq('ts_guild', true);
 
   return !!data?.[0];
