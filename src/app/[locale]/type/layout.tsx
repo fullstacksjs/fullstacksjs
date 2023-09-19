05e4c8a278fcfd96190fa55e9bf4c0a31de8825b
Alireza Safaierad <frontendmonster@gmail.com>
2023-09-19 20:44:07 +0400

    add type og

--- src/app/[locale]/type/layout.tsx
+++ src/app/[locale]/type/layout.tsx
@@ -1,6 +1,31 @@
+import type { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
 import { getServerFeature } from '@/config/features/getServerFeatures';
+
+const title = 'FullstacksJS AlphaType: Alphabet Typing Speed Competition';
+const description =
+  'FullstacksJS AlphaType: Challenge yourself by typing the alphabet as fast as you can!';
+const ogImage = {
+  url: '/og/type.png',
+  alt: 'FullstacksJS - AlphaType',
+};
+
+export const metadata: Metadata = {
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    images: ogImage,
+  },
+  twitter: {
+    title,
+    description,
+    images: ogImage,
+    card: 'summary_large_image',
+  },
+};
 
 interface Props {
   children: React.ReactNode;
