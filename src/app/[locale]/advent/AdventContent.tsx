f4e0b55c1593404985b0e634913ea83c92ea1bf2
masoudnemati <masoud.nnr@gmail.com>
2023-11-16 15:52:23 +0330

    build advent in nav

---
+++ src/app/[locale]/advent/AdventContent.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function AdventOfCodeContent() {
+  return <div>AdventOfCodeContent</div>;
+}
---
+++ src/app/[locale]/advent/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import AdventContent from './AdventContent';
+
+export default function AdventOfCodePage() {
+  return <AdventContent />;
+}
