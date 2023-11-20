d343b86428b37daf8a77368c8b510bf0cc53a098
ASafaierad <frontendmonster@gmail.com>
2023-11-20 01:05:01 +0100

    add page

---
+++ src/app/[locale]/advent/AdventOfCodeClient.ts
@@ -0,0 +1,46 @@
+export const AdventOfCodeClient = {
+  getLeaderboard() {
+    return {
+      owner_id: 3205245,
+      event: '2023',
+      members: {
+        '3205318': {
+          global_score: 0,
+          local_score: 0,
+          last_star_ts: 0,
+          id: 3205318,
+          stars: 0,
+          name: 'Amup xm', // cspell:disable-line
+          completion_day_level: {},
+        },
+        '3190265': {
+          global_score: 0,
+          local_score: 0,
+          last_star_ts: 0,
+          id: 3190265,
+          name: null,
+          stars: 0,
+          completion_day_level: {},
+        },
+        '3200952': {
+          completion_day_level: {},
+          stars: 0,
+          name: 'Atyal', // cspell:disable-line
+          global_score: 0,
+          id: 3200952,
+          last_star_ts: 0,
+          local_score: 0,
+        },
+        '3205245': {
+          completion_day_level: {},
+          name: 'ASafaeirad',
+          stars: 0,
+          global_score: 0,
+          id: 3205245,
+          last_star_ts: 0,
+          local_score: 0,
+        },
+      },
+    };
+  },
+};
