aed042be1e8c1d2b408dc82ef79870dae4e92ad9
masoudnemati <masoud.nnr@gmail.com>
2023-11-20 15:52:27 +0330

    Enhance Advent of Code announcement

--- src/app/[locale]/advent/AdventContent.tsx
+++ src/app/[locale]/advent/AdventContent.tsx
@@ -14,8 +14,27 @@ import Avatar from './Advent.png';
 
 const i18nMap: RichTranslationValues = {
   'e-tree': () => <Emoji name="tree" />,
+  'g-star': () => <Emoji name="star" />,
+  medal: () => <Emoji name="first" />,
+  robot: () => <Emoji name="robot" />,
   mark: (chunk) => <Highlight>{chunk}</Highlight>,
   b: (chunk) => <b>{chunk}</b>,
+  atype: (chunk) => (
+    <a
+      className="text-accent-0"
+      href="https://adventofcode.com/2023/leaderboard/private"
+    >
+      {chunk}
+    </a>
+  ),
+  'atype-two': (chunk) => (
+    <a
+      className="text-accent-0"
+      href="https://adventofcode.com/2023/leaderboard/private/view/3205245"
+    >
+      {chunk}
+    </a>
+  ),
 };
 
 export default function AdventOfCodeContent() {
@@ -29,6 +48,9 @@ export default function AdventOfCodeContent() {
   const t = useTranslations('advent');
 
   const title = t.rich('title', i18nMap) as React.ReactElement;
+  const howWorks = t.rich('how-works', i18nMap) as React.ReactElement;
+  const howJoin = t.rich('how-join', i18nMap) as React.ReactElement;
+  const ai = t.rich('ai', i18nMap) as React.ReactElement;
 
   return (
     <>
@@ -45,6 +67,22 @@ export default function AdventOfCodeContent() {
           <Paragraph>{t.rich('kick-off', i18nMap)}</Paragraph>
           <Paragraph>{t.rich('fun', i18nMap)}</Paragraph>
         </div>
+      </Article>
+      <Article id="advent" title={howWorks}>
+        <div>
+          <Paragraph>{t.rich('puzzles', i18nMap)}</Paragraph>
+          <Paragraph>{t.rich('stars', i18nMap)}</Paragraph>
+        </div>
+      </Article>
+      <Article id="advent" title={howJoin}>
+        <div>
+          <Paragraph>{t.rich('engage', i18nMap)}</Paragraph>
+          <Paragraph>{t.rich('already-member', i18nMap)}</Paragraph>
+          <Paragraph>{t.rich('follow', i18nMap)}</Paragraph>
+        </div>
+      </Article>
+      <Article id="ai" title={ai}>
+        <Paragraph>{t.rich('ai-des', i18nMap)}</Paragraph>
       </Article>
     </>
   );
