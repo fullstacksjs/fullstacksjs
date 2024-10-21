2058a6c41a41e2877fd24f3878d3b7473aa6568e
Alireza Safaierad <frontendmonster@gmail.com>
2024-10-21 16:03:06 +0200

    make event page static

--- src/app/[locale]/events/page.tsx
+++ src/app/[locale]/events/page.tsx
@@ -1,9 +1,16 @@
 import { generatePageOG } from '@/components/SEO';
 import { getServerFeature } from '@/config/features/getServerFeatures';
 import { getEvents } from '@/data-layer/datocms/getEvents';
+import { routing } from '@/i18n/routing';
+import { Separator } from '@radix-ui/react-dropdown-menu';
+import {
+  getTranslations,
+  unstable_setRequestLocale as setRequestLocale,
+} from 'next-intl/server';
 import { notFound } from 'next/navigation';
+import { isEmpty } from 'radash';
 
-import EventsContent from './EventContent';
+import EventList from './EventList';
 
 export const metadata = generatePageOG({
   title: 'FullstacksJS Events: Learn from the Past, Prepare for the Future',
@@ -12,10 +19,26 @@ export const metadata = generatePageOG({
   images: '/og/og.png',
 });
 
-export default async function EventsPage() {
-  if (!getServerFeature('events')) notFound();
+interface Props {
+  params: { locale: string };
+}
 
+export default async function EventsPage({ params: { locale } }: Props) {
+  if (!getServerFeature('events')) notFound();
+  const t = await getTranslations('events');
+
+  setRequestLocale(locale);
   const { upcoming, archived } = await getEvents();
 
-  return <EventsContent archived={archived} upcoming={upcoming} />;
+  return (
+    <>
+      <EventList events={upcoming} title={t('upcoming.title')} />
+      {isEmpty(upcoming) || isEmpty(archived) ? null : <Separator />}
+      <EventList events={archived} title={t('archived.title')} />
+    </>
+  );
+}
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
 }
