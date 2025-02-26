8d7e2202042135df43ebb3969b8259003c86b352
Amirreza Rahmani <amirbabble@gmail.com>
2025-02-26 16:25:57 +0330

    Convert project/route images to svg

--- src/app/[locale]/projects/+components/ProjectCard.tsx
+++ src/app/[locale]/projects/+components/ProjectCard.tsx
@@ -1,26 +1,25 @@
-import Image from 'next/image'
 import React from 'react'
 import ForwardIcon from "./ForwardIcon.svg"
 import Link from 'next/link'
 import { Project } from '../page'
 
-function ProjectCard({name , description , image , repo} : Project) {
+
+function ProjectCard({name , description , logo , repo} : Project) {
     return (
-        <div className='flex flex-col mobile:flex-col desktop:flex-row items-center gap-12 desktop:gap-16'>
-            <Image
-                height={image.height}
-                width={image.width}
-                alt={`FullstacksJS ${image.alt}`}
-                src={`/projects/${image.src}`}
-            />
-            {/* title & desc */}
+        <div className='flex flex-col mobile:flex-col desktop:flex-row items-center desktop:gap-16'>
+           <div className='w-[200px] h-[200px]'>
+            {logo}
+           </div>
             <div>
-                {/* title */}
-                <Link target='_blank' href={`https://github.com/fullstacksjs/${repo}`} className='flex items-center justify-center desktop:justify-start gap-4 text-light-0 hover:text-light-1 transition-all duration-500 '>
-                    <h2 className='font-bold text-2xl/normal text-center'>{name}</h2>
-                    <ForwardIcon />
-                </Link>
-                {/* desc */}
+            <Link 
+                 target='_blank' 
+                 href={`https://github.com/fullstacksjs/${repo}`} 
+                 className='pb-4 flex items-center justify-center desktop:justify-start gap-4 text-light-0 hover:text-light-1 transition-all duration-500'
+                >
+                   <h2 className='font-bold text-2xl/none'>{name}</h2>
+                   <ForwardIcon  />
+            </Link>
+
                 <h2 className='font-normal text-md text-light-1 text-center desktop:text-left'>
                   {description}
                 </h2>
