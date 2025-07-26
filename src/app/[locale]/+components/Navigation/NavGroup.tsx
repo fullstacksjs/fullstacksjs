2e0ff3ef3ccb118b0c8df3997fec08e601a33241
ehsanKey <ehsanKeyhani82@gmail.com>
2025-07-26 18:01:14 +0330

    fix: update Link import to use i18n routing

--- src/app/[locale]/+components/Navigation/NavGroup.tsx
+++ src/app/[locale]/+components/Navigation/NavGroup.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { comparePaths } from '@fullstacksjs/toolbox';
-import Link from 'next/link';
+import { Link } from '@/i18n/routing';
 import { useSelectedLayoutSegment } from 'next/navigation';
 
 import { CircleBadge } from '@/components/CircleBadge';
