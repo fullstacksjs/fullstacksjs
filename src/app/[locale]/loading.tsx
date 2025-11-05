72380776d01ee11b2aea34c0e3a71b85cf143056
Alireza Safaierad <frontendmonster@gmail.com>
2025-11-05 21:24:58 +0100

    add suspense boundary for /events

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -1,4 +1,6 @@
 // ISSUE: This is a workaround for Next.JS + next-intl integration [https://github.com/amannn/next-intl/issues/1493]
 export default function Loading() {
-  return <div></div>;
+  return (
+    <div className="flex justify-center items-center h-screen bg-bg-0 text-fg-0"></div>
+  );
 }
