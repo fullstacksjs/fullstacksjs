3be1f11509bbbebecd350cd640aaa95bac80387f
Alireza Safaierad <alireza.safaierad@gmail.com>
2025-12-19 12:58:12 +0100

    fix: fix canonical links

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,7 @@
 import type { Locale } from 'next-intl';
 import type { NextRequest } from 'next/server';
 
+import LinkHeader from 'http-link-header';
 import createMiddleware from 'next-intl/middleware';
 import { NextResponse } from 'next/server';
 
@@ -17,14 +18,28 @@ export async function proxy(req: NextRequest) {
 
   if (isFaOnly(req.nextUrl.pathname)) {
     const newUrl = req.url.replace('en', 'fa');
-    return NextResponse.redirect(newUrl);
+    return NextResponse.redirect(newUrl, 301);
   }
 
   const i18nMiddleware = createMiddleware({
     ...routing,
     defaultLocale: locale as Locale,
   });
-  return i18nMiddleware(req);
+  const response = i18nMiddleware(req);
+
+  const linkHeader = response.headers.get('link');
+  if (linkHeader == null) return response;
+
+  const link = LinkHeader.parse(linkHeader);
+  const defaultLinks = link.refs.find((entry) => entry.hreflang === locale);
+
+  link.refs = link.refs.map((entry) => {
+    if (entry.hreflang !== 'x-default') return entry;
+    return { ...entry, uri: defaultLinks?.uri ?? entry.uri };
+  });
+
+  response.headers.set('link', link.toString());
+  return response;
 }
 
 export const config = {
