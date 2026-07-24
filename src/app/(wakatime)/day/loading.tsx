fa36cf27899da89346c0f4b7aaee86aba734352e
Alireza Safaierad <alireza.safaierad@gmail.com>
2026-07-24 15:05:07 +0200

    implement the new day design

--- src/app/(wakatime)/day/loading.tsx
+++ src/app/(wakatime)/day/loading.tsx
@@ -1,20 +1,20 @@
-import { UserTableSkeleton } from '@/app/[locale]/wakatime/+components/UserTable';
 import { Skeleton } from '@/components/Skeleton';
-
-import Logo from './Logo.svg';
 
 export default function Loading() {
   return (
     <div className="flex flex-col gap-20">
       <div className="flex flex-col items-center gap-4">
-        <Logo />
-        <p className="text-2xl font-bold">Loading Leaderboard</p>
+        <h1 className="text-center text-4xl/none font-bold tracking-tight">
+          Wakatime
+        </h1>
+        <p className="mt-2 mb-8">
+          <Skeleton height={40} width={300} />
+        </p>
       </div>
 
       <div className="mb-10 flex flex-col gap-10 rounded-3xl">
-        <Skeleton height={400} width={900} />
+        <Skeleton height={700} width={900} />
       </div>
-      <UserTableSkeleton rows={7} />
     </div>
   );
 }
